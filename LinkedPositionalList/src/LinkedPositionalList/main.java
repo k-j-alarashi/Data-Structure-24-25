b8221d96bcd33c43cc0ae8187505aa8745161a21
Khaled Jalal Al-arashi <77218749+k-j-alarashi@users.noreply.github.com>
2025-01-29 02:07:22 +0300

    main.java

--- LinkedPositionalList/src/LinkedPositionalList/main.java
+++ LinkedPositionalList/src/LinkedPositionalList/main.java
@@ -1,13 +1,16 @@
 package LinkedPositionalList;
 
+import java.util.Iterator;
+
 public class main {
     public static void main(String[] args) {
         LinkedPositionalList<Integer> l = new LinkedPositionalList<>();
         Position<Integer> p = l.addFirst(1);
         l.addLast(3);
         l.addAfter(p,2);
-        System.out.println(l.remove(l.first()));
-        System.out.println(l.remove(l.first()));
-        System.out.println(l.remove(l.first()));
+        Iterator<Integer> i = l.iterator();
+        while(i.hasNext()){
+            System.out.println("The Elment is : "+i.next());
+        }
     }
 }
